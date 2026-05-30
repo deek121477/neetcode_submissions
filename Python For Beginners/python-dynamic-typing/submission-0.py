5695f1fcb6473eb3f623bf5f5f64be5098ef3591
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2026-05-30 17:26:46 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+
+variable=0
+print(type(variable))
+variable=0.0
+print(type(variable))
+variable=True
+print(type(variable))
+variable=""
+print(type(variable))
+variable=[]
+print(type(variable))
