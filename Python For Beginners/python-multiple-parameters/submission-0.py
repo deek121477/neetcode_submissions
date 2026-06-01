82d501ee1b64d466c1efbca692c1d0cb21d23433
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2026-06-01 08:54:52 +0530

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,12 @@
+def two_sum(a,b):
+    message = a+b
+    print(message)
+
+def three_sum(x,y,z):
+    message = x+y+z
+    print(message)
+
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
