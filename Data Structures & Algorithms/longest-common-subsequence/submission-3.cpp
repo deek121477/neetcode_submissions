fbbc4d73163316e06356cf747edccf32667f03ac
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2026-06-15 22:52:54 +0530

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int func(int ind1,int ind2,string text1, string text2,vector<vector<int>>&dp)
+{
+    if(ind1<0 || ind2<0)
+    return 0;
+  
+   if(dp[ind1][ind2]!=-1)
+   return dp[ind1][ind2];
+
+    if(text1[ind1]==text2[ind2])
+    return 1+func(ind1-1,ind2-1,text1,text2,dp);
+
+    return dp[ind1][ind2]=max(func(ind1-1,ind2,text1,text2,dp),func(ind1,ind2-1,text1,text2,dp));
+
+}
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return func(n-1,m-1,text1,text2,dp);
+    }
+};
