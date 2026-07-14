546e3e8c7b6be05a0b2f5a3fd93b96189a61bffe
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2026-07-14 16:13:06 +0530

    Add: strategy - submission-0

---
+++ Design Patterns/strategy/submission-0.cpp
@@ -0,0 +1,73 @@
+class Person {
+private:
+    string lastName;
+    int age;
+    bool married;
+
+public:
+    Person(string lastName, int age, bool married) 
+        : lastName(lastName), age(age), married(married) {}
+
+    string getLastName() {
+        return lastName;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    bool isMarried() {
+        return married;
+    }
+};
+
+class PersonFilter {
+public:
+    virtual ~PersonFilter() {}
+    virtual bool apply(Person& person) = 0;
+};
+
+class AdultFilter : public PersonFilter {
+    // Implement Adult filter
+    public:
+    bool apply(Person& person) override{
+        return person.getAge()>=18;
+    }
+};
+
+class SeniorFilter : public PersonFilter {
+    // Implement Senior filter
+    public:
+    bool apply(Person& person) override{
+        return person.getAge()>=65;
+    }
+};
+
+class MarriedFilter : public PersonFilter {
+    // Implement Married filter
+    public:
+    bool apply(Person& person) override{
+        return person.isMarried();
+    }
+};
+
+class PeopleCounter {
+private:
+    PersonFilter* filter;
+
+public:
+    void setFilter(PersonFilter& filter) {
+        this->filter = &filter;
+    }
+
+    int count(vector<Person>& people) {
+        // Implement method here
+        int cou=0;
+        for(auto& person:people){
+            if(filter->apply(person)){
+                cou++;
+            }
+        }
+        return cou;
+    }
+};
